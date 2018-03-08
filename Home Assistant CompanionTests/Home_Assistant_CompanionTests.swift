1125fc52bb2190f0db601569c67798b67c3a3a8b
Josa Gesell <josa@gesell.me>
2018-03-08 10:00:14 +0100

    Initial Commit

---
+++ Home Assistant CompanionTests/Home_Assistant_CompanionTests.swift
@@ -0,0 +1,36 @@
+//
+//  Home_Assistant_CompanionTests.swift
+//  Home Assistant CompanionTests
+//
+//  Created by Josa Gesell on 08.03.18.
+//  Copyright © 2018 Josa Gesell. All rights reserved.
+//
+
+import XCTest
+@testable import Home_Assistant_Companion
+
+class Home_Assistant_CompanionTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
